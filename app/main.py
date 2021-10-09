32b73853882f59d9a03c944f7a7772d416e769d4
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-10-09 08:40:31 +0330

    Remove some comments and rename index name

--- app/main.py
+++ app/main.py
@@ -1,18 +1,12 @@
-# Importing Flask Things
 from flask import Flask, render_template
 
-# Starting The App #
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '1234'
 
-
-# Index
 @app.route("/")
 def index():
-    return render_template("home.html")
+    return render_template("index.html")
 
-
-# App Running
 if __name__ == "__main__":
     app.run()
