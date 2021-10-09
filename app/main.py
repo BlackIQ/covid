a189a9caf5ec775cb54f0b2bf4e3049cf4c1c5d3
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-10-09 09:45:31 +0330

    Remove loop

--- app/main.py
+++ app/main.py
@@ -5,10 +5,14 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '1234'
 
-api = requests.get("https://data.covidapi.com/countries")
-api = api.json()
+api = requests.get("https://data.covidapi.com/countries").json()
+
+name = "Amirhossein"
+
+# for i in range(195):
+#     return f"<p>{api['body'][i]['country_name']}</p>"
 
 @app.route("/")
 def index():
     # return render_template("index.html")
-    return "Welcome to covid api"
+    return f"Welcome to covid api <b>{name}</b>"
