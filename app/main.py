78c2930b548c84ee79d41f0b938fb8f4f5fbdcb2
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-10-09 12:31:24 +0330

    Add value for for-loop

--- app/main.py
+++ app/main.py
@@ -11,12 +11,13 @@ api = requests.get("https://data.covidapi.com/countries").json()
 
 @app.route("/")
 def index():
-    return f"""
-        <p>
-            Country: <b>{api['body'][81]['country_name']}</b>
-            |
-            Total cases: <b>{api['body'][81]['total_cases']}</b>
-            -
-            Total deaths: <b>{api['body'][81]['total_deaths']}</b>
-        </p>
-    """
+    # return f"""
+    #     <p>
+    #         Country: <b>{api['body'][81]['country_name']}</b>
+    #         |
+    #         Total cases: <b>{api['body'][81]['total_cases']}</b>
+    #         -
+    #         Total deaths: <b>{api['body'][81]['total_deaths']}</b>
+    #     </p>
+    # """
+    return render_template("index.html", len = len(api['body']), api = api['body'])
