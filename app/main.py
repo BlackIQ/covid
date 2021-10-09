ebacbb3aa823f5ea0ea5dfe6eaf449e0ca36ca40
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-10-09 08:58:05 +0330

    Add get api

--- app/main.py
+++ app/main.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '1234'
 
+api = requests.get("https://data.covidapi.com/countries")
+api = api.json()
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    # return render_template("index.html")
+    return "Welcome to covid api"
