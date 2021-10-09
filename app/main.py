7a095b918efd434b3011661335fdb80f0dcc576a
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-10-09 10:44:58 +0330

    Testing a simple item

--- app/main.py
+++ app/main.py
@@ -7,12 +7,16 @@ app.config['SECRET_KEY'] = '1234'
 
 api = requests.get("https://data.covidapi.com/countries").json()
 
-name = "Amirhossein"
-
-# for i in range(195):
-#     return f"<p>{api['body'][i]['country_name']}</p>"
+# name = "Amirhossein"
 
 @app.route("/")
 def index():
-    # return render_template("index.html")
-    return f"Welcome to covid api <b>{name}</b>"
+    return f"""
+        <p>
+            Country: <b>{api['body'][81]['country_name']}</b>
+            |
+            Total cases: <b>{api['body'][81]['total_cases']}</b>
+            -
+            Total deaths: <b>{api['body'][81]['total_deaths']}</b>
+        </p>
+    """
